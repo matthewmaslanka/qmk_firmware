2c8ca08664c1fe8c861fd569e85c80eb45b32a9d
Matthew Maslanka <matthewmaslanka@MacBook-Pro.local>
2020-09-03 15:46:31 -0400

    Messing around with lighting

--- keyboards/ergodox_ez/keymaps/matthewmaslanka/config.h
+++ keyboards/ergodox_ez/keymaps/matthewmaslanka/config.h
@@ -1,2 +1,8 @@
-#define FORCE_NKRO
-#define COMBO_COUNT 1
+#define ORYX_CONFIGURATOR
+#undef RGBLIGHT_ANIMATIONS
+
+// Because the Macro and Function features are incompatible with Link Time Optimization, disable those features in config.h:
+//#define NO_ACTION_MACRO 
+//#define NO_ACTION_FUNCTION
+
+//#define RGBLIGHT_LAYERS
--- keyboards/ergodox_ez/keymaps/matthewmaslanka/keymap.c
+++ keyboards/ergodox_ez/keymaps/matthewmaslanka/keymap.c
@@ -1,30 +1,45 @@
+
 // Based on default keymap.
 // Layers for Sibelius, Dorico, and Finale
 // Modifiers on thumb keys where possible.
 
-#include QMK_KEYBOARD_H
+//#include QMK_KEYBOARD_H
+#include "ergodox_ez.h"
 #include "debug.h"
 #include "action_layer.h"
+#include "version.h"
 
 #define BASE 0 // default layer
 #define SHFT 1 // arrow keys, numpad, media control, display control, reset
 #define LAYR 2 // Layer hub
 #define SIB1 3 // Sibelius Base Layer
 #define SIB2 4 // Sibelius Shift Layer
-#define QWERTY 10 // QWERTY Shift layer
+#define FIN1 5 // Sibelius Base Layer
+#define FIN2 6 // Sibelius Shift Layer
 
-
-
-
-enum combos {
-  QZ_A
+#define QRT 10 // QWERTY Shift layer
+enum my_keycodes{
+    MENU_LH = SAFE_RANGE // Keyboard Maestro Palette Left Hand
 };
 
-const uint16_t PROGMEM qz_combo[] = {KC_Q, KC_Z, COMBO_END};
 
-combo_t key_combos[COMBO_COUNT] = {
-  [QZ_A] = COMBO(qz_combo, KC_A)
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case MENU_LH:
+      if (record->event.pressed) {
+        SEND_STRING(SS_LALT("`")); // Triggers Keyboard Maestro "Sibelius LH" Macro Group Palette
+        layer_on(QRT); // Switch to QWERTY Layer on press
+      } 
+      else {
+        layer_off(QRT); // Turn off QWERTY Layer
+        layer_on(SIB1);
+        SEND_STRING(SS_DOWN(X_LGUI) SS_DOWN(X_LALT) SS_DOWN(X_LSFT) "`" SS_UP(X_LGUI) SS_UP(X_LALT) SS_UP(X_LSFT)); // Close Palettes
+      }
+      return false; // Skip all further processing of this key      
+  }
+  return true; // Process all other keycodes normally
 };
+
 
 
 
@@ -36,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,      KC_E,     KC_R,   KC_T,   KC_BSPC,
         KC_NO,          KC_A,         KC_S,      KC_D,     KC_F,   KC_G,
         MO(SHFT),        KC_Z,         KC_X,      KC_C,     KC_V,   KC_B,   KC_ENT,
-        KC_GRV,         KC_NO,        KC_LBRC,   KC_RBRC,  KC_SPC,
+        KC_GRV,         RGB_M_K,        KC_LBRC,   KC_RBRC,  KC_SPC,
                                               MT(MOD_LALT | MOD_LGUI, KC_NO),  MT(MOD_LALT | MOD_LGUI | MOD_LSFT, KC_NO),
                                                               KC_LCTL,
                                                MT(MOD_LGUI,KC_SPC),MT(MOD_LSFT,KC_ENT),KC_LALT,
@@ -45,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_BSPC,        KC_Y,    KC_U,   KC_I,     KC_O,     KC_P,      KC_EQL,
                              KC_H,    KC_J,   KC_K,     KC_L,     KC_SCLN,   KC_QUOT,
              KC_ENT,         KC_N,    KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,   MO(SHFT),
-                             KC_LEFT, KC_UP,  KC_DOWN,  KC_RIGHT, KC_NO,
+                             KC_LEFT, KC_UP,  KC_DOWN,  KC_RIGHT, KC_BSLASH,
              MT(MOD_LALT | MOD_LGUI | MOD_LSFT, KC_NO),        MT(MOD_LALT | MOD_LGUI, KC_NO),
              KC_LCTL,
              KC_LALT,MT(MOD_LSFT,KC_ENT),MT(MOD_LGUI,KC_SPC)
@@ -70,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_KP_0,  _______,  KC_KP_0,  _______,  KC_KP_ENTER,
        _______, _______,
        _______,
-       _______, _______, _______
+       RGB_SAI, RGB_HUI, RGB_VAI
 ),
 
 // Layer Switch
@@ -78,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // left hand
        _______, _______, _______, _______, _______, _______, _______,
        _______, _______, _______, _______, _______, _______, _______,
-       _______, _______, TO(SIB1),_______, _______, _______,
+       _______, _______, TO(SIB1),_______, TO(FIN1),_______,
        _______, _______, _______, _______, _______, _______, _______,
        _______, _______, _______, _______, _______,
                                            _______, _______,
@@ -99,21 +114,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // left hand
        _______,   _______, _______, KC_D,    KC_P,    LGUI(KC_S), TO(BASE),
        _______,   _______, _______, KC_UP,   _______, _______,    _______,
-       _______,   _______, KC_LEFT, KC_DOWN, KC_RIGHT,KC_H,
-       MO(QWERTY),_______, _______, _______, _______, KC_BSLASH,  _______,
+       _______,    _______, KC_LEFT, KC_DOWN, KC_RIGHT,KC_H,
+       MO(QRT),   _______, _______, _______, _______, KC_BSLASH,  MENU_LH,
        _______,   KC_F,    _______, _______, _______,
                                              _______, _______,
                                                       _______,
-                                    _______, _______, _______,
+                                    KC_LGUI, _______, _______,
     // right hand
        _______, KC_1,     KC_2,     _______,  _______,  KC_4,     KC_ESC,
        _______, KC_R,     KC_KP_7,  KC_KP_8,  KC_KP_9,  KC_T,     _______,
                 _______,  KC_KP_4,  KC_KP_5,  KC_KP_6,  _______,  _______,
-       _______, _______,  KC_KP_1,  KC_KP_2,  KC_KP_3,  _______,  MO(SIB2),
+       MENU_LH, _______,  KC_KP_1,  KC_KP_2,  KC_KP_3,  _______,  MO(SIB2),
                           KC_KP_0,  KC_Z,     KC_KP_DOT,KC_F,     KC_KP_ENTER,
        _______, _______,
        _______,
-       _______, _______, _______
+       _______, _______, KC_RGUI
 ),
 // Sibelius Shift
 [SIB2] = LAYOUT_ergodox(
@@ -137,7 +152,50 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        _______, _______, _______
 ),
 
-[QWERTY] = LAYOUT_ergodox(  // layer 0 : default
+// Finale Base
+[FIN1] = LAYOUT_ergodox(
+    // left hand
+       _______,   _______, _______, _______, _______, LGUI(KC_S), TO(BASE),
+       _______,   _______, _______, KC_UP,   _______, _______,    _______,
+       _______,   _______, KC_LEFT, KC_DOWN, KC_RIGHT,KC_K,
+       MO(QRT),   _______, _______, _______, _______, KC_M,       MENU_LH,
+       _______,   _______, _______, _______, _______,
+                                             _______, _______,
+                                                      _______,
+                                    KC_LGUI, _______, _______,
+    // right hand
+       _______, KC_1,     KC_2,     _______,  _______,  KC_4,     KC_ESC,
+       _______, KC_R,     KC_KP_7,  KC_KP_8,  KC_KP_9,  KC_T,     _______,
+                _______,  KC_KP_4,  KC_KP_5,  KC_KP_6,  _______,  _______,
+       MENU_LH, _______,  KC_KP_1,  KC_KP_2,  KC_KP_3,  _______,  MO(SIB2),
+                          KC_KP_0,  KC_Z,     KC_KP_DOT,KC_F,     KC_KP_ENTER,
+       _______, _______,
+       _______,
+       _______, _______, KC_RGUI
+),
+// Finale Shift
+[FIN2] = LAYOUT_ergodox(
+    // left hand
+       _______, _______, _______, _______, _______, _______, _______,
+       _______, _______, _______, _______, _______, _______, _______,
+       _______, _______, _______, _______, _______, _______,
+       _______, _______, _______, _______, _______, _______, _______,
+       _______, _______, _______, _______, _______,
+                                           _______, _______,
+                                                    _______,
+                                  _______, _______, _______,
+    // right hand
+       _______,  _______, _______, _______, _______, _______, _______,
+       _______,  _______, _______, KC_UP, _______, _______, _______,
+                 _______, KC_LEFT, KC_DOWN, KC_RIGHT,_______, _______,
+       _______,  _______, _______, _______, _______, _______, _______,
+                          _______, _______, _______, _______, _______,
+       _______, _______,
+       _______,
+       _______, _______, _______
+),
+
+[QRT] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_GESC,        KC_1,         KC_2,      KC_3,     KC_4,   KC_5,   MO(LAYR),
         KC_TAB,         KC_Q,         KC_W,      KC_E,     KC_R,   KC_T,   KC_BSPC,
@@ -159,9 +217,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
+
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
-
 };
 
 // Runs constantly in the background, in a loop.
@@ -174,16 +232,19 @@ void matrix_scan_user(void) {
   ergodox_right_led_2_off();
   ergodox_right_led_3_off();
   switch (layer) {
-    // TODO: Make this relevant to the ErgoDox EZ.
     case SHFT:
       ergodox_right_led_1_on();
       break;
     case SIB1:
       ergodox_right_led_2_on();
       break;
+    case FIN1:
+      ergodox_right_led_3_on();
+      break;
     default:
       // none
       break;
   }
-
 };
+
+
